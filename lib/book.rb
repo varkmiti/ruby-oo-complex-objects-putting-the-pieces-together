9bb82957eb4a29f386b32ea768f2979f5fc45ade
Mark Viti <+>
2022-05-30 15:39:35 -0400

    commit tree c573cc9ec3fbacb66290d3733ef83ed5f624b92c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book 
+    def initialize(title)
+        @title = title 
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+end
 
 
